21881ac15bf050c66851fba0ba2ca7b28542caa9
Andrew Pisarevsky <renakdup@gmail.com>
2020-04-26 15:08:38 +0300

    Рефакторинг genDiff метода, в частности переработана логика сравнения файлов на AST дерево с последующим преобразованием его в строку. Добавлена PHPUnit библиотека и написаны тесты для метода generateAstDiff()

--- public/inc/Gendiff.php
+++ public/inc/Gendiff.php
@@ -13,34 +13,89 @@ function genDiff(string $pathToFile1, string $pathToFile2): string
     $file1 = file_get_contents($pathToFile1);
     $file2 = file_get_contents($pathToFile2);
 
+    $astDiff = generateAstDiff($file1, $file2);
+    $result = outputDiff($astDiff);
+
+    return $result;
+}
+
+function generateAstDiff(string $file1, string $file2): array
+{
+    $result = [];
+
     $parsedFile1 = json_decode($file1, true);
     $parsedFile2 = json_decode($file2, true);
 
-    $renderLine = function ($key, $val, string $sign = ' '): string {
-        $val = is_bool($val) ? var_export($val, true) : $val;
-        return "  {$sign} {$key}: {$val}";
-    };
-
-    $lines = [];
-
     foreach ($parsedFile1 as $key => $val) {
         if (isset($parsedFile2[$key]) && $parsedFile2[$key] === $val) {
-            $lines[] = $renderLine($key, $val);
+            $result[$key] = [
+                'operator' => '=',
+                'value' => $val
+            ];
             unset($parsedFile2[$key]);
         } elseif (isset($parsedFile2[$key]) && $parsedFile2[$key] !== $val) {
-            $lines[] = $renderLine($key, $parsedFile2[$key], '+');
-            $lines[] = $renderLine($key, $val, '-');
+            $result[$key] = [
+                [
+                    'operator' => '+',
+                    'value' => $parsedFile2[$key],
+                ],
+                [
+                    'operator' => '-',
+                    'value' => $val,
+                ],
+            ];
             unset($parsedFile2[$key]);
         } elseif (! isset($parsedFile2[$key])) {
-            $lines[] = $renderLine($key, $val, '-');
+            $result[$key] = [
+                'operator' => '-',
+                'value' => $val
+            ];
         }
     }
 
-    $lostFile2Lines = collect($parsedFile2)->map(function ($val, $key) use ($renderLine) {
-        return $renderLine($key, $val, '+');
+    $lostFile2Lines = collect($parsedFile2)->map(function ($val, $key) {
+        return [
+            'operator' => '+',
+            'value' => $val
+        ];
     })->all();
 
-    $lines = array_merge($lines, $lostFile2Lines);
+    $result = array_merge($result, $lostFile2Lines);
+
+    return $result;
+}
+
+function getDiffLines(array $diff, ?string $parentKey = null): array
+{
+    $lines = [];
+
+    foreach ($diff as $key => $item) {
+        if (is_array($item) && ! isset($item['operator'])) {
+            $lines = array_merge($lines, getDiffLines($item, $key));
+            continue;
+        }
+
+        if (isset($parentKey)) {
+            $lines[] = renderLine($parentKey, $item['value'], $item['operator']);
+        } else {
+            $lines[] = renderLine($key, $item['value'], $item['operator']);
+        }
+    }
+
+    return $lines;
+}
+
+function renderLine($key, $val, string $sign): string
+{
+    $sign = $sign === '=' ? ' ' : $sign;
+
+    $val = is_bool($val) ? var_export($val, true) : $val;
+    return "  {$sign} {$key}: {$val}";
+}
+
+function outputDiff(array $astDiff): string
+{
+    $lines = getDiffLines($astDiff);
 
     return "{" . PHP_EOL . implode(PHP_EOL, $lines) . PHP_EOL . "}" . PHP_EOL;
 }
---
+++ tests/fixtures/files/second/result.php
@@ -0,0 +1,16 @@
+<?php
+
+return [
+    'host' => [
+        'operator' => '+',
+        'value' => 'hexlet.io',
+    ],
+    'timeout' => [
+        'operator' => '+',
+        'value' => 20,
+    ],
+    'verbose' => [
+        'operator' => '+',
+        'value' => true
+    ]
+];
