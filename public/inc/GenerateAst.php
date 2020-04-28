16e6236a1e36c8b2d037236084a440aa4d17bc4e
Andrew Pisarevsky <renakdup@gmail.com>
2020-04-29 00:11:39 +0300

    Правки по алгоритму генерации AST дерева, теперь рекурсивно сравнивает файлы уходя вглубь

--- public/inc/GenerateAst.php
+++ public/inc/GenerateAst.php
@@ -4,11 +4,68 @@ declare(strict_types=1);
 
 namespace Renakdup\GenerateAst;
 
-function generateAstDiff(array $dataBefore, array $dataAfter): array
+//function generateAstDiff(array $dataBefore, array $dataAfter): array
+//{
+//    $result = [];
+//
+//    foreach ($dataBefore as $key => $val) {
+//        if (isset($dataAfter[$key]) && $dataAfter[$key] === $val) {
+//            $result[$key] = [
+//                'operator' => '=',
+//                'value' => $val
+//            ];
+//            unset($dataAfter[$key]);
+//        } elseif (isset($dataAfter[$key]) && $dataAfter[$key] !== $val) {
+//            $result[$key] = [
+//                [
+//                    'operator' => '+',
+//                    'value' => $dataAfter[$key],
+//                ],
+//                [
+//                    'operator' => '-',
+//                    'value' => $val,
+//                ],
+//            ];
+//            unset($dataAfter[$key]);
+//        } elseif (! isset($dataAfter[$key])) {
+//            $result[$key] = [
+//                'operator' => '-',
+//                'value' => $val
+//            ];
+//        }
+//    }
+//
+//    $lostLines = collect($dataAfter)->map(function ($val, $key) {
+//        return [
+//            'operator' => '+',
+//            'value' => $val
+//        ];
+//    })->all();
+//
+//    $result = array_merge($result, $lostLines);
+//
+//    return $result;
+//}
+
+
+
+function generateAstDiff(array $dataBefore, array $dataAfter, ?string $parentKey = null): array
 {
     $result = [];
 
     foreach ($dataBefore as $key => $val) {
+        if (is_array($val) && ! isset($dataAfter[$key])) {
+            $result[$key] = [
+                'operator' => '-',
+                'value' => $val
+            ];
+            continue;
+        } elseif (is_array($val)) {
+            $result[$key] = generateAstDiff($dataBefore[$key], $dataAfter[$key]);
+            unset($dataAfter[$key]);
+            continue;
+        }
+
         if (isset($dataAfter[$key]) && $dataAfter[$key] === $val) {
             $result[$key] = [
                 'operator' => '=',
